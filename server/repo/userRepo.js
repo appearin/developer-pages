f8bd08ef0dd99bbbf053b6efdd0edbca1f9a5c52
Sigurd Grøneng <sigurd@appear.in>
2016-06-20 09:20:41 +0200

    Made classes for needed users db operations

---
+++ server/repo/userRepo.js
@@ -0,0 +1,33 @@
+var knex = require('../dbconfig');
+
+class UserRepo {
+
+  static saveUser(user){
+    if(!this.isValid(user)){
+      console.log('rejecting because invalid user');
+      return Promise.reject('Cannot save invalid user');
+    }
+
+    return knex('users')
+    .insert({
+      user: user.name,
+      email: user.email,
+      domain: user.domain
+    })
+    .returning('id')
+    .catch(function(error){
+        console.log(error);
+    });
+  };
+
+  static isValid(user){
+    if(!user || !user.name || !user.email || !user.domain){
+      return false;
+    }
+    console.log("Saving user");
+    return true;
+  };
+
+}
+
+module.exports = UserRepo;

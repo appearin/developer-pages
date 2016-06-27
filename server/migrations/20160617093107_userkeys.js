ca60b856cd821e172bde3d1556244098dceac66b
Sigurd Grøneng <sigurd@appear.in>
2016-06-27 13:44:18 +0200

    Gave table columns better names

--- server/migrations/20160617093107_userkeys.js
+++ server/migrations/20160617093107_userkeys.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('users', function(table){
-    table.increments('id').primary();
-    table.string('user');
+    table.increments('userId').primary();
+    table.string('name');
     table.string('email');
     table.string('domain');
   });
